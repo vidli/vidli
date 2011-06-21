9215fba373d920037de8b1addb1bf62cec2409cc
nshb <nshb@nshbrown.com>
2011-06-21 10:37:12 -0700

    Open Source Video eCommerce Platform

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidli::Application.config.secret_token = '73c072da9159c7354bda43f8ddf20a8ab8bfe3d0a44475ecc602df5919e5a524cb1c859b0dc9fc3635869d00d7681c8405202df5b4db02aca08cb9c93f3d8d53'
